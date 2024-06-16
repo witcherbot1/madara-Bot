c490b90c7c152ab7da713401898315bb6964d75a
HARLEY LIGHT <158737422+omar-lite@users.noreply.github.com>
2024-06-16 12:08:13 +0300

    Update تغير_الاسم.js

--- plugins/تغير_الاسم.js
+++ plugins/تغير_الاسم.js
@@ -1,12 +1,12 @@
 let handler  = async (m, { conn, args, text }) => {
-if (!text) throw `*[❗اشعار❗] مثال : .تغير-الاسم 𝐍𝐀𝐑𝐎𝐓𝐔 👑 𝐁𝐎𝐓*`
+if (!text) throw `*[❗اشعار❗] مثال : .تغير-الاسم 𝐌𝐀𝐃𝐀𝐑𝐀↳🐢↲𝐁𝐎𝐓*`
 try {
 let text = args.join` `
 if(!args || !args[0]) {
 } else {
 conn.groupUpdateSubject(m.chat, text)}
 } catch (e) {
-throw '*[❗اشعار❗] *مثال : .تغيرالاسم 𝐍𝐀𝐑𝐎𝐓𝐔 👑 𝐁𝐎𝐓*'
+throw '*[❗اشعار❗] *مثال : .تغيرالاسم 𝐌𝐀𝐃𝐀𝐑𝐀↳🐢↲𝐁𝐎𝐓*'
 }}
 handler.help = ['setname <text>']
 handler.tags = ['group']
