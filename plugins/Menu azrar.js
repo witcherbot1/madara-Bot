dd68ef3a06f4e82ba099e2873f67e202428dad94
HARLEY LIGHT <158737422+omar-lite@users.noreply.github.com>
2024-06-16 07:15:28 +0300

    Add files via upload

---
+++ plugins/Menu azrar.js
@@ -0,0 +1,163 @@
+function clockString(ms) {
+    let h = Math.floor(ms / 3600000);
+    let m = Math.floor(ms % 3600000 / 60000);
+    let s = Math.floor(ms % 60000 / 1000);
+    return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':');
+}
+
+import pkg from '@whiskeysockets/baileys';
+const { generateWAMessageFromContent, proto, prepareWAMessageMedia } = pkg;
+
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+    let d = new Date(new Date + 3600000);
+    let locale = 'ar';
+    let week = d.toLocaleDateString(locale, { weekday: 'long' });
+    let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' });
+    let _uptime = process.uptime() * 1000;
+    let uptime = clockString(_uptime);
+    let user = global.db.data.users[m.sender];
+    let name = conn.getName(m.sender)
+    let { money, joincount } = global.db.data.users[m.sender];
+    let { exp, limit, level, role } = global.db.data.users[m.sender];
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length;
+    let more = String.fromCharCode(8206);
+    let readMore = more.repeat(850);
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+  await conn.sendMessage(m.chat, { react: { text: '👑', key: m.key } })
+  const harley = 'https://telegra.ph/file/97c7983b872fa1bc4abf7.jpg'
+ 
+conn.relayMessage(m.chat, { viewOnceMessage: { message: { interactiveMessage: { header: { title: `alucard`}, body: { text: `*مــرحــبــا بــك/ي يــا ${m.pushName}*
+╗──────¤مــعلـ🛖ـومــاتــك
+> •  ▫️اســمــك : ${name}
+> •  ▫️مــســتــواك : ${level}
+> •  ▫️بــريــمــيــوم : ${user.premiumTime > 0 ? 'مــمـ🔱ـيز' : (isPrems ? 'مــمـ🔱ـيز' : 'عــ🍁ــادي') || ''}
+> •  ▫️رتــبــتــك : ${role}
+╝───────────────¤
+╗──────¤مــعلــومــات الــ🤖بــوت
+> •  ▫️إســم الــبــوت: نــاروتــ👑ــو
+> •  ▫️الــمــطــور : هــارلــي لايــت
+> •  ▫️مــدة الــتــشــغــيل : ${uptime}
+> •  ▫️عــدد الــمــســجــلــيــن : ${rtotalreg}
+╝───────────────¤
+*🚨:الرجاء الضغط علي زر القوائم لي رؤية القائمة الخاصة بل البوت*
+> Copyright © 2024 Harley`,subtitle: "HARLEY",},header: { hasMediaAttachment: true,...(await prepareWAMessageMedia({ image : { url: harley } }, { upload: conn.waUploadToServer }, {quoted: m}))},nativeFlowMessage: { buttons: [
+
+
+                            {
+                                name: 'single_select',
+                                buttonParamsJson: JSON.stringify({
+                                    title: '⌈🛡╎الــقــوائـــم╎🛡⌋',
+                                    sections: [
+                                        {
+                                            title: 'مــرحـ🛡ـبــا بــك فـي مــ☑هــام نـاروتـو بـ🤖ـوت',
+                                            highlight_label: 'بعبص براحتك يابرو 🤖',
+                                            rows: [
+                                                {
+                                                    header: 'الــقـ👑ـســم الـاول',
+                                                    title: 'استدعاء_قسم_الاعضاء #الاعضاء',
+                                                    description: '',
+                                                    id: '.م1'
+                                                },
+                                                {
+                                                    header: 'الــقـ👨🏻‍💻ـســم الــثــانــي',
+                                                    title: 'استدعاء_قسم_المشرفين #المشرفين',
+                                                    description: '',
+                                                    id: '.م11'
+                                                },
+                                                {
+                                                    header: 'الــقـ🕋ـســم الــثــالــث',
+                                                    title: 'استدعاء_قسم_الدين #الدين',
+                                                    description: '',
+                                                    id: '.م2'
+                                                },
+                                                {
+                                                    header: 'الــقـ👑ـســم الــرابــع',
+                                                    title: 'استدعاء_قسم_المطور #المطور',
+                                                    description: '',
+                                                    id: '.م3'
+                                                },
+                                                {
+                                                    header: 'الــقـ🛡ـســم الــخــامــس',
+                                                    title: 'استدعاء_قسم_التنزيلات #التنزيلات',
+                                                    description: '',
+                                                    id: '.م4'
+                                                },
+                                                {
+                                                    header: 'الــقـ🕹ـســم الــســادس',
+                                                    title: 'استدعاء_قسم_الالعاب #الالعاب',
+                                                    description: '',
+                                                    id: '.م5'
+                                                },
+                                                {
+                                                    header: 'الــقـ🌀ـســم الــســابــع',
+                                                    title: 'استدعاء_قسم_التحويلات #التحويلات',
+                                                    description: '',
+                                                    id: '.م6'
+                                                },
+                                                {
+                                                    header: 'الــقـ🎧ـســم الــثــامـن',
+                                                    title: 'استدعاء_قسم_الصوتيات #الصوتيات',
+                                                    description: '',
+                                                    id: '.م7'
+                                                },
+                                                {
+                                                    header: 'الــقـ🤖ـســم الــتــاســع',
+                                                    title: 'استدعاء_قسم_الذكاء #الذكاء',
+                                                    description: '',
+                                                    id: '.م8'
+                                                },
+                                                {
+                                                    header: 'الــقـ🚨ـســم الــعــاشــر',
+                                                    title: 'استدعاء_قسم_الدعم #الدعم',
+                                                    description: '',
+                                                    id: '.م9'
+                                                },
+                                                {
+                                                    header: 'الــقـ🔍ـســم الــحــاديــة عــشــر',
+                                                    title: 'استدعاء_قسم_ابحث #البحث',
+                                                    description: '',
+                                                    id: '.م12'
+                                                },
+                                                {
+                                                    header: 'الــقـ📽️ـســم الــثــانــي عــشــر',
+                                                    title: 'استدعاء_قسم_الافلام #الافلام',
+                                                    description: '',
+                                                    id: '.م13'
+                                                },
+                                                {
+                                                    header: 'الــقـ👨🏻‍💻ـســم الــثــانــي عــشــر',
+                                                    title: 'سكربت_بوت_جاهز_للتعديل_👨🏻‍💻',
+                                                    description: '',
+                                                    id: '.م10'
+                                               }
+                                            ]
+                                        }
+                                    ]
+                                }),
+                  messageParamsJson: ''
+                },
+                {
+              name: "quick_reply",
+              buttonParamsJson: '{"display_text":"⌈👑╎مـطـور الـبـوت╎👑⌋","id":".المطور"}'
+            },
+            {
+               name: "cta_url",
+               buttonParamsJson: '{"display_text":"⌈👨🏻‍💻╎مـوقــع المـطــور╎👨🏻‍💻⌋","url":"https://www.atom.bio/harley_light","merchant_url":"https://www.atom.bio/harley_light"}'
+                     },
+                     {
+               name: "cta_url",
+               buttonParamsJson: '{"display_text":"⌈📲╎قـنـاة الـمـطـور╎📲⌋","url":"https://whatsapp.com/channel/0029VagvImBA2pLLSI7lLE1B","merchant_url":"https://whatsapp.com/channel/0029VaXddtu0lwgiREisx82C"}'
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+    }, {});
+}
+
+handler.help = ['info'];
+handler.tags = ['main'];
+handler.command = ['menu', 'مهام', 'اوامر','الاوامر']
+
+export default handler;
