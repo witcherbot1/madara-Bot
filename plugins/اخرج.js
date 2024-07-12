ac57fe872591cce6c91271688b52e597c2e5673b
HARLEY LIGHT <158737422+omar-lite@users.noreply.github.com>
2024-07-13 00:10:41 +0300

    Delete plugins directory

--- plugins/اخرج.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn, text, command }) => {
-let id = text ? text : m.chat  
-await conn.reply(id, '*لقد قضيت معكم وقتاً رائعاً الى اللقاء ⁦^⁠_⁠^⁩*') 
-await conn.groupLeave(id)}
-handler.command = /^(اخرج|اطلع|غادر|خروج)$/i
-handler.group = true
-handler.rowner = true
-export default handler
